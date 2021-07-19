4b1bb8acef8bc210604c1a86b7eb1a4215dd6b66
HAYASHITAKUMI <86405682+HAYASHITAKUMI@users.noreply.github.com>
2021-07-19 16:22:19 +0900

    修正１

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
@@ -22,19 +23,15 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
-    # @tweet.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
-    # @prototype = Prototype.new(prototype_params)
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -43,7 +40,6 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
@@ -52,18 +48,16 @@ class PrototypesController < ApplicationController
   end
 
 
-private
+  private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def move_to_index
-    redirect_to root_path 
-    unless
-      current_user == @prototype.user 
-    end
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
-
-
-
